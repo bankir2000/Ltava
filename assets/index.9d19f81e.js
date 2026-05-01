2e31060827396c42197cc655af7491ed09f90017
bankir2000 <162804434+bankir2000@users.noreply.github.com>
2026-05-01 11:50:34 +0300

    Change greeting from 'Hello World' to 'Goodbye World'

--- assets/index.9d19f81e.js
+++ assets/index.9d19f81e.js
@@ -3883,12 +3883,8 @@ const Lc = Io(Tc, [["render", Fc]])
             return {
                 title: "\u041F\u043E\u0432\u0456\u0434\u043E\u043C\u043B\u0435\u043D\u043D\u044F \u043F\u0440\u043E \u0432\u0438\u044F\u0432\u043B\u0435\u043D\u043D\u044F \u0446\u0456\u043B\u0456",
                 text: [
-    (() => {
-  if (!this.form.time) return "";
-  const [datePart, timePart] = this.form.time.split(" ");
-  const [year, month, day] = datePart.split("-");
-  return `Дата: ${day}.${month}.${year}\nЧас: ${timePart}`;
-})(),
+                    this.form.date ?  `Дата: ${this.form.date}` : "",
+       this.form.time ?  `Час: ${this.form.time}` : "",
     this.form.sign ? this.form.sign : "",
     this.form.nearestCity ? `Н.П. ${this.form.nearestCity}` : "",
     this.form.target ? this.form.target : "",
@@ -4048,7 +4044,9 @@ const Lc = Io(Tc, [["render", Fc]])
       month: "2-digit",
       year: "numeric"
     });
-    this.form.time = `${date} ${time}`;
+    this.form.date = `${date}`;
+    this.form.time = `${time}`;
+
 },
         getCoordinates() {
             navigator.geolocation ? navigator.geolocation.getCurrentPosition(e=>{
