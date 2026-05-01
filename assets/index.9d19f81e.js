8b885491ffe699fde75e3994abcb68389b527561
bankir2000 <162804434+bankir2000@users.noreply.github.com>
2026-05-01 11:16:41 +0300

    Update print statement to say 'Goodbye World'

--- assets/index.9d19f81e.js
+++ assets/index.9d19f81e.js
@@ -3883,13 +3883,13 @@ const Lc = Io(Tc, [["render", Fc]])
             return {
                 title: "\u041F\u043E\u0432\u0456\u0434\u043E\u043C\u043B\u0435\u043D\u043D\u044F \u043F\u0440\u043E \u0432\u0438\u044F\u0432\u043B\u0435\u043D\u043D\u044F \u0446\u0456\u043B\u0456",
                 text: [
-    this.form.time ? ("" + this.form.time).replace(":", ".") : "",
+    this.form.time ? ("Дата: " + this.form.time).replace(":", ".") : "",
     this.form.sign ? this.form.sign : "",
-    this.form.nearestCity ? `р-н н.п. ${this.form.nearestCity}` : "",
+    this.form.nearestCity ? `Н.П. ${this.form.nearestCity}` : "",
     this.form.target ? this.form.target : "",
     this.form.target_side ? this.form.target_side : "",
     this.form.target_description ? this.form.target_description : "",
-    this.form.tcil ? `№ ${this.form.tcil}.` : "",
+    this.form.tcil ? `№ цілі: ${this.form.tcil}.` : "",
     this.form.disclosure ? this.form.disclosure : "",
     this.form.number_of_targets ? `Кількість: ${this.form.number_of_targets}од.` : "",
     (() => {
